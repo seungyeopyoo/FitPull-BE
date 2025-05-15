fe1ec3c14573f6ef5a6a6228cfe908f0b00bdd29
yooseungyeop <yooseungyub@naver.com>
2025-05-15 20:34:26 +0900

    fix/ add SWAGGER_SERVER_URL swagger.js에서 환경 변수 사용으로 서버 URL 재설정

--- src/app.js
+++ src/app.js
@@ -17,39 +17,47 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//스웨거
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/api-docs', (req, res) => {
+  res.redirect('/api-docs/');
+});
+
 // 라우트 설정
 app.use("/api/auth", authRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/categories", categoryRouter);
 
-// 스웨거 설정
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 // 기본 라우트
 app.get("/", (_, res) => {
-	res.send("백엔드 서버 정상 작동 중.");
+        res.send("백엔드 서버 정상 작동 중.");
 });
 
 const PORT = Number(process.env.PORT || "3000", 10);
 
 // 서버 시작
 app.listen(PORT, "0.0.0.0", () => {
-	console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
+        console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
 });
 
 // Prisma 연결 테스트
 prisma
-	.$connect()
-	.then(() => {
-		console.log("DB 연결 성공");
-	})
-	.catch((err) => {
-		console.error("DB 연결 실패:", err);
-	});
+        .$connect()
+        .then(() => {
+                console.log("DB 연결 성공");
+        })
+        .catch((err) => {
+                console.error("DB 연결 실패:", err);
+        });
 
 // 서버 종료 시 Prisma 연결 해제
 process.on("SIGINT", async () => {
-	await prisma.$disconnect();
-	process.exit(0);
+        await prisma.$disconnect();
+        process.exit(0);
+});
+
+app.get('/swagger.json', (_, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
 });
