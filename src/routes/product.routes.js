c629dcb6b721f6d4382036a4aabf165d2b3de8e5
yooseungyeop <yooseungyub@naver.com>
2025-05-15 17:51:45 +0900

    feat/swagger-docs

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -6,6 +6,67 @@ import {
 	getProductsByUserController,
 } from "../controllers/product.controller.js";
 
+/**
+ * @swagger
+ * tags:
+ *   name: Product
+ *   description: 상품 등록 / 조회 API
+ */
+
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: 상품 등록
+ *     tags: [Product]
+ *     responses:
+ *       201:
+ *         description: 상품 등록 성공
+ */
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: 전체 상품 조회
+ *     tags: [Product]
+ *     responses:
+ *       200:
+ *         description: 상품 목록 반환
+ */
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: 상품 상세 조회
+ *     tags: [Product]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: 상품 ID
+ *     responses:
+ *       200:
+ *         description: 상품 상세 정보 반환
+ */
+
+/**
+ * @swagger
+ * /products/my/{userId}:
+ *   get:
+ *     summary: 내가 등록한 상품 조회
+ *     tags: [Product]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         description: 유저 ID
+ *     responses:
+ *       200:
+ *         description: 내 상품 리스트 반환
+ */
+
 const router = express.Router();
 //상품등록
 router.post("/", createProductController);
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -6,6 +6,46 @@ import {
 } from "../controllers/user.controller.js";
 import { authenticate } from "../middlewares/auth.js";
 
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: 내 정보 관리 API
+ */
+
+/**
+ * @swagger
+ * /users/me:
+ *   get:
+ *     summary: 내 정보 조회
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: 내 프로필 반환
+ */
+
+/**
+ * @swagger
+ * /users/me:
+ *   patch:
+ *     summary: 내 정보 수정
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: 수정된 프로필 반환
+ */
+
+/**
+ * @swagger
+ * /users/me:
+ *   delete:
+ *     summary: 회원 탈퇴
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: 회원 탈퇴 완료
+ */
+
 const router = express.Router();
 //내정보조회
 router.get("/me", authenticate, getMyProfile);
