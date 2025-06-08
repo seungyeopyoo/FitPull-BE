abdbdba851c171cd125a14dc9f898399b73f832c
yooseungyeop <yooseungyub@naver.com>
2025-06-09 04:33:06 +0900

    fix/swagger-comment-auth

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -13,7 +13,7 @@ const options = {
     },
     servers: [
       {
-        url: process.env.SWAGGER_SERVER_URL || 'http://localhost:3000',
+        url: 'http://localhost:3000',
       },
     ],
   },
