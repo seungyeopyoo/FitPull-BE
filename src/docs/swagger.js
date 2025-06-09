20c3649f004c26cfd93fb10d36e43efd6957bd98
yooseungyeop <yooseungyub@naver.com>
2025-06-10 00:13:46 +0900

    fix/swagger-url-rollback

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -13,7 +13,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: process.env.SWAGGER_SERVER_URL || 'http://localhost:3000',
       },
     ],
     tags: [
